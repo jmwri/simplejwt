020fcbc1fc85ce43452daf4b45e94e0a66d2d993
Jim Wright <jim@linx.net>
2018-03-21 09:32:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# simplejwt
+
+A dead simple JWT library.
+
+# Running tests
+## Install the package with test dependencies
+`pip install -e ".[test]"`
+
+## Run tox
+`tox`
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+github = 'https://github.com/jmwri/simplejwt'
+version = '0.0.1'
+
+setup(
+    name='simplejwt',
+    packages=find_packages(),
+    version=version,
+    license='MIT',
+    python_requires='>=3.6, <4',
+    description='A dead simple JWT library',
+    author='Jim Wright',
+    author_email='jmwri93@gmail.com',
+    url=github,
+    download_url='{github}/archive/{version}.tar.gz'.format(
+        github=github, version=version
+    ),
+    keywords=['python', 'jwt', 'simple', 'simplejwt'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    setup_requires=['pytest-runner'],
+    install_requires=[
+        'typing',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    extras_require={
+        'test': [
+            'coverage', 'tox'
+        ]
+    }
+)
