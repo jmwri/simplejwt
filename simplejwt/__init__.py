66c7694950dbb0e5e30149e1cd91f20b98eadb62
Jim Wright <jim@linx.net>
2018-04-19 08:38:08 +0100

    Allow import of Jwt from root level

--- simplejwt/__init__.py
+++ simplejwt/__init__.py
@@ -1,4 +1,4 @@
-from simplejwt.jwt import encode, make, decode, compare_signature, \
+from simplejwt.jwt import Jwt, encode, make, decode, compare_signature, \
     compare_token
 
 __version__ = '0.5.0'
